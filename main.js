f554b672d30e545df8d4d3b24e65b73d646db806
yash-jivani <yashjivani17@gmail.com>
2022-02-05 01:34:08 +0530

    nav-bar v0.1.0

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
- 
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0];
+// console.log(toggleBtn);
+
+const navLinks = document.getElementsByClassName('nav-links')[0];
+// console.log(navLinks);
+
+toggleBtn.addEventListener('click',()=>{
+    navLinks.classList.toggle('active');
+})
